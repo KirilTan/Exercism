629d567c0447e31cd2ae6160255b6d0ca4c406c9
kiril <kiriltanev@proton.me>
2024-11-03 10:21:25 +0200

    feat: exercise solution added

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,9 @@
-def square(number):
-    pass
+def square(number: int) -> int:
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+    return 2 ** (number - 1)
 
-
-def total():
-    pass
+def total(board_size: int = 64) -> int:
+    if board_size < 1 or board_size > 64:
+        raise ValueError("square must be between 1 and 64")
+    return 2 ** board_size - 1
