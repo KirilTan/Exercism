2f693313a4c11dcb898e7964567a620b0ab43bdd
kiril <kiriltanev@proton.me>
2024-11-05 19:20:03 +0200

    feat: exercise solution added

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,8 @@
-def is_armstrong_number(number):
-    pass
+def is_armstrong_number(number: int) -> bool:
+    power_of = len(str(number))
+    total_sum = 0
+
+    for current_number in str(number):
+        total_sum += pow(int(current_number), power_of)
+
+    return total_sum == int(number)
