450b2a581bfdefdf60da532dbb0e185b2fd07623
kiril <kiriltanev@proton.me>
2024-11-08 08:44:06 +0200

    feat: func added

--- python/little-sisters-vocab/strings.py
+++ python/little-sisters-vocab/strings.py
@@ -56,7 +56,7 @@ def remove_suffix_ness(word: str) -> str:
     return new_string[:-1] + 'y' if new_string.endswith('i') else new_string
 
 
-def adjective_to_verb(sentence: str, index: int) -> str:
+def adjective_to_verb(sentence: str, index: int, suffix: str = 'en') -> str:
     """Change the adjective within the sentence to a verb.
 
     :param sentence: str - that uses the word in sentence.
@@ -66,4 +66,4 @@ def adjective_to_verb(sentence: str, index: int) -> str:
     For example, ("It got dark as the sun set.", 2) becomes "darken".
     """
 
-    pass
+    return sentence.split(' ')[index].replace('.','') + suffix
