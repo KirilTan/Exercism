0cb11c6c356571a8f165a0e4e054a58becf94a32
Kiril <kiriltanev@pm.me>
2025-03-14 16:49:45 +0200

    feat: solution added

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,13 @@
-def score(x, y):
-    pass
+import math
+
+def score(x: float, y: float) -> int:
+    distance = math.sqrt(x ** 2 + y ** 2)
+
+    if distance <= 1:
+        return 10  # Inner circle
+    elif distance <= 5:
+        return 5  # Middle circle
+    elif distance <= 10:
+        return 1  # Outer circle
+    else:
+        return 0  # Outside the target
