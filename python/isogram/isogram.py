bf94c8119ed951ae0da9745b64d67a3e58240134
Kiril <kiriltanev@pm.me>
2025-12-29 23:13:40 +0200

    feat: isogram solution added

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,5 @@
-def is_isogram(string):
-    pass
+def is_isogram(string: str) -> bool:
+    letters = [ch.lower() for ch in string if ch.isalpha()]
+    return len(letters) == len(set(letters))
+
+print(is_isogram('Emily Jung Schwartzkopf'))
